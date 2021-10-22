1777258a69604e9d496b46bc4e83a06643da4217
Ziritto <ziritto@gmail.com>
2021-10-22 12:52:23 +0200

    z dnia 22-10-2021

--- pythonnauka.py
+++ pythonnauka.py
@@ -21,3 +21,53 @@ print('{:_>20}'.format(slowo))
 print('{:_^20}'.format(slowo))
 print('{:>20.5}'.format(slowo))
 print('{:+d}'.format(liczba))
+
+#zadanie 4
+zad = "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+print(dir(zad))
+help(zad.center)
+
+#zadanie 5
+imie2 = "Filip"[::-1]
+nazwisko2 = "Warpechowski"[::-1]
+print(imie2)
+print(nazwisko2)
+print("\n")
+
+#zadanie 6
+lista = [1,3,5,7,9,2,4,6,8,10]
+lista2 = lista[5:]
+print(lista2)
+del lista[5:]
+print(lista)
+print("\n")
+
+#zadanie 7
+lista.extend(lista2)
+print(lista)
+lista.insert(0,0)
+print(lista)
+kopia = lista.copy()
+print(kopia)
+lista.sort()
+print(lista)
+print("\n")
+
+#zadanie 8
+
+index = (123456,234567,345678,456789,567890)
+im_i_naz = ("Filip warpechowski","Olaf Nowak", "Zenek Martyniuk","Michał Wiśniewski","Marta Łapszo")
+dane = index, im_i_naz
+print(dane[0][0],dane[1][0])
+print(dane[0][1],dane[1][1])
+print(dane[0][2],dane[1][2])
+print(dane[0][3],dane[1][3])
+print(dane[0][4],dane[1][4])
+print("\n")
+
+#zadanie 9
+
+
+
+
+
